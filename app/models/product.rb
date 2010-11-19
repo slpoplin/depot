2040a436233f21368ea1ed488c6fa22708ade72e
Spencer Poplin <spencerz@gmail.com>
2010-11-18 23:08:22 -0500

    Iteration T - Add Product unit tests.

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,7 +7,7 @@ class Product < ActiveRecord::Base
   validate :price_must_be_at_least_a_cent
   validates_uniqueness_of :title
   validates_format_of :image_url,
-                      :with => %r{\.(git|jpg|png)$}i,
+                      :with => %r{\.(gif|jpg|png)$}i,
                       :message => 'must be a URL for GIF, JPG or PNG image.'
 
   def self.find_products_for_sale
--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -1,8 +1,49 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test "invalid with empty attributes" do
+    product = Product.new
+    assert !product.valid?
+    assert product.errors.invalid?(:title)
+    assert product.errors.invalid?(:description)
+    assert product.errors.invalid?(:price)
+    assert product.errors.invalid?(:image_url)
+  end
+  
+  test "positive price" do
+    product = Product.new(:title        => "My Book Title",
+                          :description  => "slp",
+                          :image_url    => "slp.jpg")
+    product.price = -1
+    assert !product.valid?
+    assert_equal "should be at least 0.01", product.errors.on(:price)
+    
+    product.price = 0
+    assert !product.valid?
+    assert_equal "should be at least 0.01", product.errors.on(:price)
+    
+    product.price = 1
+    assert product.valid?
+  end
+  
+  test "image_url" do
+    ok = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg http://a.b.c/x/y/z/fred.gif }
+    bad = %w{ fred.doc fred.gif/more fred.gif.more }
+    
+    ok.each do |name|
+      product = Product.new(:title       => "My Book Title",
+                            :description => "slp",
+                            :price       => 1,
+                            :image_url   => name)
+      assert product.valid?, product.errors.full_messages
+    end
+    
+    bad.each do |name|
+      product = Product.new(:title       => "My Book Title",
+                            :description => "slp",
+                            :price       => 1,
+                            :image_url   => name)
+      assert !product.valid?, "saving #{name}"
+    end
   end
 end
